d4494c551b40ceaca90084bef00fad73a6f01516
eloudsa <said.eloudrhiri@lampiris.be>
2019-09-14 21:38:51 +0200

    Test publication of the project

--- test/ChainBizzTest.js
+++ test/ChainBizzTest.js
@@ -80,7 +80,65 @@ contract('ChainBizz', async accounts => {
     assert.equal(
       project['_status'],
       ProjectStatus.Draft,
-      'published must be ' + ProjectStatus.Draft
+      'status must be ' + ProjectStatus.Draft
+    );
+  });
+
+  it('should let us publish a project', async () => {
+    // publish the project
+    const receipt = await contractInstance.publishProject(projectId, {
+      from: accounts[1]
+    });
+
+    // check that we have received an event
+    assert.equal(receipt.logs.length, 1, 'should have received 1 event');
+    assert.equal(
+      receipt.logs[0].event,
+      'PublishedProject',
+      'event name should be PublishedProject'
+    );
+    assert.equal(
+      receipt.logs[0].args._id.toNumber(),
+      projectId,
+      'project id must be 1'
+    );
+    assert.equal(
+      receipt.logs[0].args._owner,
+      accounts[1],
+      'customer must be ' + accounts[1]
+    );
+    assert.equal(
+      receipt.logs[0].args._title,
+      title,
+      'project title must be ' + title
+    );
+    assert.equal(receipt.logs[0].args._price, price, 'price must be ' + price);
+
+    // retrieve the project from the contract
+    const project = await contractInstance.getProject(projectId);
+
+    // check that we have properly stored the project
+    assert.equal(
+      project['_owner'],
+      accounts[1],
+      'customer must be ' + accounts[1]
+    );
+    assert.equal(
+      web3.utils.toBN(project['_provider']),
+      0,
+      'provider must be null'
+    );
+    assert.equal(project['_title'], title, 'project title must be ' + title);
+    assert.equal(
+      project['_description'],
+      description,
+      'description must be ' + description
+    );
+    assert.equal(project['_price'], price, 'price must be ' + price);
+    assert.equal(
+      project['_status'],
+      ProjectStatus.Published,
+      'status must be ' + ProjectStatus.Published
     );
   });
 });
