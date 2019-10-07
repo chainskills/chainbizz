c9a5743d76d3586d36f96ec568dffe6e793ed7b7
eloudsa <said.eloudrhiri@lampiris.be>
2019-10-07 20:37:10 +0900

    Transform class to function with hooks

--- client/src/components/ContractData/Project/ProjectContractData.js
+++ client/src/components/ContractData/Project/ProjectContractData.js
@@ -1,82 +1,75 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default class ProjectContractData extends Component {
-  constructor(props) {
-    super(props);
+const ProjectContractData = ({ projectId, drizzle, drizzleState }) => {
+  const [dataKey, setDataKey] = useState(null);
 
-    this.state = this.props.drizzle.store.getState();
-    const account = this.props.drizzleState.accounts[0];
+  useEffect(() => {
+    const account = drizzleState.accounts[0];
 
-    const { ChainBizz } = this.props.drizzle.contracts;
-    this.dataKey = ChainBizz.methods.getProject.cacheCall(
-      this.props.projectId,
-      {
+    const { ChainBizz } = drizzle.contracts;
+    setDataKey(
+      ChainBizz.methods.getProject.cacheCall(projectId, {
         from: account
-      }
+      })
     );
+
+    //eslint-disable-next-line
+  }, []);
+
+  const account = drizzleState.accounts[0];
+  // Retrieve project details only if the information is available
+  let projectDetails = null;
+  if (
+    drizzleState.contracts.ChainBizz.getProject[dataKey] &&
+    drizzleState.contracts.ChainBizz.getProject[dataKey].value
+  ) {
+    projectDetails = drizzleState.contracts.ChainBizz.getProject[dataKey].value;
   }
 
-  render() {
-    const account = this.props.drizzleState.accounts[0];
-    // Retrieve project details only if the information is available
-    let projectDetails = null;
-    if (
-      this.props.drizzleState.contracts.ChainBizz.getProject[this.dataKey] &&
-      this.props.drizzleState.contracts.ChainBizz.getProject[this.dataKey].value
-    ) {
-      projectDetails = this.props.drizzleState.contracts.ChainBizz.getProject[
-        this.dataKey
-      ].value;
-    }
+  // project not yet ready or not found
+  if (projectDetails === null || typeof projectDetails === 'undefined') {
+    return <span>Initializing...</span>;
+  }
 
-    // project not yet ready or not found
-    if (projectDetails === null || typeof projectDetails === 'undefined') {
-      return <span>Initializing...</span>;
-    }
-
-    const id = this.props.projectId;
-
-    console.log('Owner: ' + projectDetails._owner);
-    console.log('Account: ' + this.props.drizzleState.accounts[0]);
-
-    return (
-      <div className='col s12 m4'>
-        <div className='card medium blue-grey'>
-          <div className='card-content white-text'>
-            <span className='card-title'>{projectDetails._title}</span>
-            <p>
-              <span className='badge blue white-text'>
-                {this.props.drizzle.web3.utils.fromWei(
-                  projectDetails._price.toString(),
-                  'ether'
-                )}
-                {' ETH'}
-              </span>
-            </p>
-            <p>{projectDetails._description}</p>
-          </div>
-          <div className='card-action'>
-            {projectDetails._owner === account && (
-              <a href='#!'>
-                <i className='material-icons'>delete</i>
-              </a>
-            )}
-            {projectDetails._owner === account && (
-              <a href='#!'>
-                <i className='material-icons'>edit</i>
-              </a>
-            )}
-            {projectDetails._owner === account && (
-              <a href='#!'>
-                <i className='material-icons'>publish</i>
-              </a>
-            )}
+  return (
+    <div className='col s12 m4'>
+      <div className='card medium blue-grey'>
+        <div className='card-content white-text'>
+          <span className='card-title'>{projectDetails._title}</span>
+          <p>
+            <span className='badge blue white-text'>
+              {drizzle.web3.utils.fromWei(
+                projectDetails._price.toString(),
+                'ether'
+              )}
+              {' ETH'}
+            </span>
+          </p>
+          <p>{projectDetails._description}</p>
+        </div>
+        <div className='card-action'>
+          {projectDetails._owner === account && (
             <a href='#!'>
-              <i className='material-icons'>more_horiz</i>
+              <i className='material-icons'>delete</i>
             </a>
-          </div>
+          )}
+          {projectDetails._owner === account && (
+            <a href='#!'>
+              <i className='material-icons'>edit</i>
+            </a>
+          )}
+          {projectDetails._owner === account && (
+            <a href='#!'>
+              <i className='material-icons'>publish</i>
+            </a>
+          )}
+          <a href='#!'>
+            <i className='material-icons'>more_horiz</i>
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default ProjectContractData;
