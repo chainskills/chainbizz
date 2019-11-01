da50544775618d39a585496f5953af1c1bc3d7d4
eloudsa <said.eloudrhiri@lampiris.be>
2019-11-01 11:12:13 +0100

    project detail rework

--- client/src/components/pages/project/ProjectDetail.js
+++ client/src/components/pages/project/ProjectDetail.js
@@ -4,6 +4,8 @@ import { createBrowserHistory } from 'history';
 
 import ProjectContext from '../../context/projects/projectContext';
 
+import './ProjectDetail.css';
+
 const ProjectDetail = ({ match, drizzle, drizzleState, account }) => {
   const projectContext = useContext(ProjectContext);
   const { getProject, current } = projectContext;
@@ -52,19 +54,136 @@ const ProjectDetail = ({ match, drizzle, drizzleState, account }) => {
       <div>
         <div className='row'>
           <div className='col s12 m12'>
+            <div className='single__header'>
+              <div className='row'>
+                <div className='col s2'>
+                  <div className='single__meta'>
+                    <span className='single__metaTitle'>Payout</span>
+                    <span className='single__metaValue'>2 ETH</span>
+                    <span className='single__metaSubvalue'>180€</span>
+
+                    <span className='single__metaTitle'>Remaining Balance</span>
+                    <span className='single__metaValue'>4 ETH</span>
+                    <span className='single__metaSubvalue'>360€</span>
+                  </div>
+                </div>
+                <div className='col s10'>
+                  <h1
+                    style={{
+                      fontSize: '24px',
+                      margin: '0',
+                      fontWeight: 'bold',
+                      marginBottom: '10px'
+                    }}
+                  >
+                    <a href='#' style={{ color: '#676767' }}>
+                      Streamix [STRX] - BitcoinTalk ANN thread Comment
+                    </a>
+                  </h1>
+                  <div>
+                    <span
+                      className='new badge'
+                      style={{ float: 'inherit', margin: '0 5px 0 0' }}
+                    >
+                      text
+                    </span>
+                    <span
+                      className='new badge'
+                      style={{ float: 'inherit', margin: '0 5px 0 0' }}
+                    >
+                      text
+                    </span>
+                    <span
+                      className='new badge'
+                      style={{ float: 'inherit', margin: '0 5px 0 0' }}
+                    >
+                      text
+                    </span>
+                  </div>
+                  <code className=' language-markup'>flow-text</code>
+                  <div style={{ marginTop: '20px' }}>
+                    <img
+                      src='https://www.abeautifulsite.net/uploads/2014/08/bit-face.png?width=600&key=c6d70b7b067981cded2d49fc8a5e3ca1dc9dc9fdaab2ac05db4cb96481a36a77'
+                      className='left'
+                      style={{
+                        width: '40px',
+                        height: '40px',
+                        borderRadius: '100%',
+                        verticalAlign: 'top'
+                      }}
+                    />
+                    <p
+                      className='truncate'
+                      style={{
+                        position: 'relative',
+                        top: '7px',
+                        width: '130px',
+                        paddingLeft: '10px'
+                      }}
+                    >
+                      0xe11e30dea3459deab3219ee32a14ec3400c89
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col s12 m12'>
+          <div className='card card-custom'>
+            <div className='card-content'>
+              <div className='row'>
+                <div className='single'>
+                  <div className='single__content'>
+                    <div className='row'>
+                      <div className='col s9'>
+                        <div>
+                          <p>
+                            Esta tarefa é para os alunos do curso de Blockchain
+                            apoiados pelo Banco Mundial na vila olímpica de
+                            Acari, com organização da Blockchain Academy e
+                            patrocínio da Maker DAO.
+                          </p>
+                        </div>
+                      </div>
+                      <div className='col s3'>
+                        <div className='project-additional'>
+                          <ul style={{ margin: '-10px 0 0 0' }}>
+                            <li style={{ fontWeight: '600' }}>
+                              <i className='material-icons'>
+                                perm_data_setting
+                              </i>
+                              Beginner <span>difficulty</span>
+                            </li>
+                            <li style={{ fontWeight: '600' }}>
+                              <i className='material-icons'>history</i>5{' '}
+                              <span>days remaining</span>
+                            </li>
+                            <li style={{ fontWeight: '600' }}>
+                              <i className='material-icons'>thumb_up</i>0{' '}
+                              <span>submissions</span>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s12 offset-s10'>
             <a
-              href='#!'
-              className='col s12 m2 waves-effect waves-light btn-small left secondary-content action'
+              className='waves-effect waves-light btn blue-grey lighten-1 no-uppercase'
+              style={{ width: '150px' }}
               onClick={() => goBack()}
-              style={{ margin: '5px' }}
             >
-              Go back
+              Close
             </a>
           </div>
         </div>
-        <h5>{project.title}</h5>
-        <p>{project.description}</p>
-        <p>{project.price} ETH</p>
       </div>
     </div>
   );
