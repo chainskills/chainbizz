ecb71d153f9f912c548aa31d09012d9b3280a2b9
eloudsa <said.eloudrhiri@lampiris.be>
2019-11-01 19:08:14 +0100

    remove unused object

--- client/src/App.js
+++ client/src/App.js
@@ -117,7 +117,6 @@ const App = ({ drizzleContext }) => {
                     render={({ match }) => (
                       <ProjectDetail
                         drizzle={drizzle}
-                        drizzleState={drizzleState}
                         account={account}
                         match={match}
                       />
