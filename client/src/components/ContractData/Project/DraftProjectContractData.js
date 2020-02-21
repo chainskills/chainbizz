9af698d3561951c59e79f7ff0015c6dbfcc5566d
Said Eloudrhiri <saideloudrhiri@lam-1380.local>
2020-02-21 18:00:28 +0100

    chips

--- client/src/components/ContractData/Project/DraftProjectContractData.js
+++ client/src/components/ContractData/Project/DraftProjectContractData.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import JazzIcon, { jsNumberForAddress } from 'react-jazzicon';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import JazzIcon, { jsNumberForAddress } from "react-jazzicon";
+import { NavLink } from "react-router-dom";
 
-import ActionsOwner from './ActionsOwner';
-import ActionsProvider from './ActionsProvider';
+import ActionsOwner from "./ActionsOwner";
+import ActionsProvider from "./ActionsProvider";
 
-import { projectStatus } from './ProjectStatus';
+import { projectStatus } from "./ProjectStatus";
 
-import './ProjectContractData.css';
+import styles from "./ProjectContractData.css";
 
 const DraftProjectContractData = ({
   projectId,
@@ -17,111 +17,118 @@ const DraftProjectContractData = ({
   hideAction = false
 }) => {
   // project not yet ready or not found
-  if (projectDetails === null || typeof projectDetails === 'undefined') {
+  if (projectDetails === null || typeof projectDetails === "undefined") {
     return <span>Initializing...</span>;
   }
 
   return (
-    <div className='col s12 m12'>
-      <div className='card card-custom sticky-action'>
-        <div className='card-content'>
+    <div className="col s12 m12">
+      <div className="card styles.card-custom sticky-action">
+        <div className="card-content">
           <div>
-            <div className='row'>
-              <div className='col s12 m12'>
-                <span className='card-title activator grey-text text-darken-4'>
+            <div className="row">
+              <div className="col s12 m12">
+                <span className="card-title activator grey-text text-darken-4">
                   <NavLink
-                    activeClassName='chosen'
+                    activeClassName="chosen"
                     to={`/project/draft/${projectId}`}
                   >
                     {projectDetails.title}
                   </NavLink>
-                  <i className='material-icons right'>more_vert</i>
+                  <i className="material-icons right">more_vert</i>
                 </span>
               </div>
-              <div className='col s12 m6'>
+              <div className="col s12 m6">
                 <div>
                   <span
-                    className='new badge'
-                    style={{ float: 'inherit', margin: '0 5px 0 0' }}
+                    className="new badge"
+                    style={{ float: "inherit", margin: "0 5px 0 0" }}
                   >
                     text
                   </span>
                   <span
-                    className='new badge'
-                    style={{ float: 'inherit', margin: '0 5px 0 0' }}
+                    className="new badge"
+                    style={{ float: "inherit", margin: "0 5px 0 0" }}
                   >
                     text
                   </span>
                   <span
-                    className='new badge'
-                    style={{ float: 'inherit', margin: '0 5px 0 0' }}
+                    className="new badge"
+                    style={{ float: "inherit", margin: "0 5px 0 0" }}
                   >
                     text
                   </span>
+
+                  <div className="chips">
+                    <div className="chip">
+                      Toto
+                      <i className="close material-icons">close</i>
+                    </div>
+                  </div>
                 </div>
-                <div style={{ marginTop: '20px' }} className='avatar'>
+                <div style={{ marginTop: "20px" }} className="avatar">
                   <JazzIcon
                     diameter={40}
                     seed={jsNumberForAddress(projectDetails.issuer)}
                   />
                   <p
-                    className='truncate'
+                    className="truncate"
                     style={{
-                      position: 'relative',
-                      top: '7px',
-                      width: '130px',
-                      paddingLeft: '10px'
+                      position: "relative",
+                      top: "7px",
+                      width: "130px",
+                      paddingLeft: "10px"
                     }}
                   >
                     {projectDetails.issuer}
                   </p>
                 </div>
               </div>
-              <div className='col s12 m3 project-additional'>
-                <ul style={{ margin: '-10px 0 0 0' }}>
-                  <li style={{ fontWeight: '600' }}>
-                    <i className='material-icons'>perm_data_setting</i>Beginner{' '}
+              <div className="col s12 m3 project-additional">
+                <ul style={{ margin: "-10px 0 0 0" }}>
+                  <li style={{ fontWeight: "600" }}>
+                    <i className="material-icons">perm_data_setting</i>Beginner{" "}
                     <span>difficulty</span>
                   </li>
-                  <li style={{ fontWeight: '600' }}>
-                    <i className='material-icons'>history</i>5{' '}
+                  <li style={{ fontWeight: "600" }}>
+                    <i className="material-icons">history</i>5{" "}
                     <span>days remaining</span>
                   </li>
-                  <li style={{ fontWeight: '600' }}>
-                    <i className='material-icons'>thumb_up</i>0{' '}
+                  <li style={{ fontWeight: "600" }}>
+                    <i className="material-icons">thumb_up</i>0{" "}
                     <span>submissions</span>
                   </li>
                 </ul>
               </div>
-              <div className='col s12 m3 project-additional'>
+              <div className="col s12 m3 project-additional">
                 <p
-                  className='right-align'
-                  style={{ fontSize: '24px', color: '#546e7a' }}
+                  className="right-align"
+                  style={{ fontSize: "24px", color: "#546e7a" }}
                 >
                   {drizzle.web3.utils.fromWei(
                     projectDetails.price.toString(),
-                    'ether'
-                  )}{' '}
+                    "ether"
+                  )}{" "}
                   ETH
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className='card-reveal'>
-          <span className='card-title grey-text text-darken-4'>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4">
             <NavLink
-              activeClassName='chosen'
+              activeClassName="chosen"
               to={`/project/draft/${projectId}`}
             >
               {projectDetails.title}
             </NavLink>
-            <i className='material-icons right'>close</i>
+            <i className="material-icons right">close</i>
           </span>
           <p>{projectDetails.description}</p>
         </div>
         {hideAction === false && (
-          <div className='card-action right-align project-card'>
+          <div className="card-action right-align project-card">
             {projectDetails.issuer === account && (
               <ActionsOwner
                 projectId={projectId}
