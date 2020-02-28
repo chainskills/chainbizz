e2d06dc9ca0bee0818d2afa096dc47920258d96a
Said Eloudrhiri <said@noratek.net>
2020-02-28 20:08:40 +0100

    Ratings forms

--- client/src/components/dialog/modal/ratings/RatingsIssuerModal.js
+++ client/src/components/dialog/modal/ratings/RatingsIssuerModal.js
@@ -9,7 +9,7 @@ import '../Modal.css';
 import starempty from '../../../../assets/images/starempty.png';
 import starfull from '../../../../assets/images/starfull.png';
 
-const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
+const RatingsIssuerModal = ({ dataID, onClose, action1, action2 }) => {
   const escPress = useKeyPress('Escape');
 
   const [ratings, setRatings] = useState([3, 3, 3, 3, 3, 3]);
@@ -34,14 +34,14 @@ const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
     <div className='modalEx-overlay'>
       <div className='modalEx'>
         <div className='modal-content'>
-          <h4>Set ratings to the fulfiller</h4>
+          <h4>Set ratings to the issuer</h4>
           <p>
-            Please review your collaboration with your fulfiller by providing a
+            Please review your collaboration with your issuer by providing a
             score between 1 to 5 (1: strongly disagree, 5: highly agree):
           </p>
           <div>
             <div className='row'>
-              <div className='col s6'>My project was delivered on-time:</div>
+              <div className='col s6'>The requests were clearly described and precise:</div>
               <div className='col s6'>
                 <Rating
                   emptySymbol={
@@ -55,8 +55,11 @@ const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
                 />
               </div>
             </div>
+            
             <div className='row'>
-              <div className='col s6'>My project was delivered on-budget:</div>
+              <div className='col s6'>
+                The issuer has the right soft-skills:
+              </div>
               <div className='col s6'>
                 <Rating
                   emptySymbol={
@@ -66,14 +69,12 @@ const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
                     <img src={starfull} className='icon ratingsIcon' />
                   }
                   onChange={value => onChangeRating(1, value)}
-                  initialRating={ratings[1]}
+                  initialRating={ratings[3]}
                 />
               </div>
             </div>
             <div className='row'>
-              <div className='col s6'>
-                The fulfiller has the right hard-skills:
-              </div>
+              <div className='col s6'>The requests and the communication with the issuer were serious:</div>
               <div className='col s6'>
                 <Rating
                   emptySymbol={
@@ -83,14 +84,12 @@ const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
                     <img src={starfull} className='icon ratingsIcon' />
                   }
                   onChange={value => onChangeRating(2, value)}
-                  initialRating={ratings[2]}
+                  initialRating={ratings[4]}
                 />
               </div>
             </div>
             <div className='row'>
-              <div className='col s6'>
-                The fulfiller has the right soft-skills:
-              </div>
+              <div className='col s6'>I will recommend this issuer:</div>
               <div className='col s6'>
                 <Rating
                   emptySymbol={
@@ -100,36 +99,6 @@ const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
                     <img src={starfull} className='icon ratingsIcon' />
                   }
                   onChange={value => onChangeRating(3, value)}
-                  initialRating={ratings[3]}
-                />
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col s6'>The quality have been meet:</div>
-              <div className='col s6'>
-                <Rating
-                  emptySymbol={
-                    <img src={starempty} className='icon ratingsIcon' />
-                  }
-                  fullSymbol={
-                    <img src={starfull} className='icon ratingsIcon' />
-                  }
-                  onChange={value => onChangeRating(4, value)}
-                  initialRating={ratings[4]}
-                />
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col s6'>I will recommend this fulfiller:</div>
-              <div className='col s6'>
-                <Rating
-                  emptySymbol={
-                    <img src={starempty} className='icon ratingsIcon' />
-                  }
-                  fullSymbol={
-                    <img src={starfull} className='icon ratingsIcon' />
-                  }
-                  onChange={value => onChangeRating(5, value)}
                   initialRating={ratings[5]}
                 />
               </div>
@@ -161,4 +130,4 @@ const RatingsFulfillerModal = ({ dataID, onClose, action1, action2 }) => {
   );
 };
 
-export default RatingsFulfillerModal;
+export default RatingsIssuerModal;
